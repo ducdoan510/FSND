803bca4a3b9783c4b2de57461ac70147c573f90b
duc.doan <3dmailboxes@gmail.com>
2020-07-24 23:07:07 +0800

    update error handler name

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -229,7 +229,7 @@ def create_app(test_config=None):
         }), 404
 
     @app.errorhandler(400)
-    def not_found(error):
+    def bad_request(error):
         return jsonify({
             "success": False,
             "error": 400,
@@ -237,7 +237,7 @@ def create_app(test_config=None):
         }), 400
 
     @app.errorhandler(422)
-    def not_found(error):
+    def unprocessable(error):
         return jsonify({
             "success": False,
             "error": 422,
@@ -245,7 +245,7 @@ def create_app(test_config=None):
         }), 422
 
     @app.errorhandler(500)
-    def not_found(error):
+    def internal_server_error(error):
         return jsonify({
             "success": False,
             "error": 500,
